5e42756eecc3fec1ec88253268ecef6e5052872b
Maximus <max.brereton@gmail.com>
2018-05-04 14:13:20 +1000

    Clean user input to remove potentially malicious items

--- backend/app.py
+++ backend/app.py
@@ -12,9 +12,18 @@ def hello():
 
 @app.route('/api/user-ratings/<username>')
 def get_mal_user_ratings(username):
-    req = requests.get('https://myanimelist.net/malappinfo.php?status=all&type=anime&u=' + username)
+    escaped_username = escapeUserInput(username)
+    req = requests.get('https://myanimelist.net/malappinfo.php?status=all&type=anime&u=' + escaped_username)
     data = json.dumps(xmltodict.parse(req.text))
     return data
+
+def escapeUserInput(userInput):
+    userInput = userInput.replace('&', '&amp;')
+    userInput = userInput.replace('<', '&lt;')
+    userInput = userInput.replace('>', '&gt;')
+    userInput = userInput.replace('\"', '&quot;')
+    userInput = userInput.replace('\'', '&apos;')
+    return userInput
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, threaded=True)
