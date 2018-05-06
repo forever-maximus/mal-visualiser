01f9b0b8ea5fd2706014d7c7c9dc8056d1c0e8f0
Maximus <max.brereton@gmail.com>
2018-05-06 15:41:00 +1000

    alter minor config to prepare initial deploy on server

--- frontend/src/mal-api.js
+++ frontend/src/mal-api.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
-const baseAddress = 'http://127.0.0.1:8000/api/user-ratings/';
+const isProd = false;
+let baseAddress = '';
+
+if (isProd) {
+  baseAddress = 'http://172.104.162.144/api/user-ratings/';
+} else {
+  baseAddress = 'http://127.0.0.1/api/user-ratings/';
+}
 
 export function get_user_ratings(username) {
   return new Promise((resolve, reject) => {
